048956b4823d87f0740055948a6c02d63d64c4bc
femoriam <f.amorim@outlook.com.br>
2018-12-28 21:40:35 -0200

    Adicionado function de select e busca

--- index.php
+++ index.php
@@ -2,17 +2,31 @@
 
 require_once("config.php");
 
-/* Exemplo aula 1 $sql = new Sql();
+/* Exemplo - Carrega todos */
+/* $sql = new Sql();
 
 $usuarios = $sql->select("SELECT * FROM tb_usuarios");
 
 echo json_encode($usuarios); */
 
-$tony = new Usuario();
-
+/* Exemplo - Carrega somente um*/
+/*$tony = new Usuario();
 $tony->loadById(2);
+echo $tony;*/
 
-echo $tony;
 
+/* Exemplo - Carrega uma lista de usuarios */
+/* $lista = Usuario::getList();
+echo json_encode($lista); */
+
+/* Exemplo - Busca usuario */
+/*$busca = Usuario::search("to");
+echo json_encode($busca);*/
+
+/* Exemplo - Buscando usuario com login e senha */
+$usuario = new Usuario();
+$usuario->login("tony", "123");
+
+echo $usuario;
 
 ?>
